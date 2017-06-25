73f4de68ed38cb7c4fb471f5ce87aba23669c5ae
nayakshweta <nayakshweta07@gmail.com>
2017-06-25 23:51:52 +0530

    Step 2 : Improves the code a little bit. Uses a class instead of a function.

--- webframework.py
+++ webframework.py
@@ -1,9 +1,13 @@
-def application(environ, start_response):
-    """Simplest possible application object"""
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
-    return ['Hello world!\n']
+class application:
+    def __init__(self, environ, start_response):
+        self.environ = environ
+        self.start = start_response
+    
+    def __iter__(self):
+        status = '200 OK'
+        response_headers = [('Content-type', 'text/plain')]
+        self.start(status, response_headers)
+        yield 'Hello world!\n'
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
