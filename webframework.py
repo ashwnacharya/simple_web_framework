51837b36b4e86a9c259f606fe165be9577cda250
nayakshweta <nayakshweta07@gmail.com>
2017-06-26 00:05:33 +0530

    Step 3 : Supports 2 hardcoded URLS, "/" and "/hello". Returns error for any other URL.

--- webframework.py
+++ webframework.py
@@ -4,10 +4,31 @@ class application:
         self.start = start_response
     
     def __iter__(self):
+        path = self.environ['PATH_INFO']
+        if path == "/":
+            return self.GET_index()
+        elif path == "/hello":
+            return self.GET_hello()
+        else:
+            return self.notfound()
+
+    def GET_index(self):
         status = '200 OK'
         response_headers = [('Content-type', 'text/plain')]
         self.start(status, response_headers)
-        yield 'Hello world!\n'
+        yield "Welcome!\n"
+
+    def GET_hello(self):
+        status = '200 OK'
+        response_headers = [('Content-type', 'text/plain')]
+        self.start(status, response_headers)
+        yield "Hello world!\n"
+
+    def notfound(self):
+        status = '404 Not Found'
+        response_headers = [('Content-type', 'text/plain')]
+        self.start(status, response_headers)
+        yield "Not Found\n"
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
