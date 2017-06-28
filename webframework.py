08479c3ca32afd84596d36d0f1c565ff6c9107d3
nayakshweta <nayakshweta07@gmail.com>
2017-06-28 23:29:44 +0530

    Step 5 : Parameterizing the URLs. Performs regex match to handle the URLs.

--- webframework.py
+++ webframework.py
@@ -1,3 +1,5 @@
+import re
+
 class application:
     def __init__(self, environ, start_response):
         self.environ = environ
@@ -5,18 +7,22 @@ class application:
     
     urls = [
         ("/", "index"),
-        ("/hello", "hello")
+        ("/hello/(.*)", "hello")
     ]
 
     def __iter__(self):
-        path_info = self.environ['PATH_INFO']
+        path = self.environ['PATH_INFO']
         method = self.environ['REQUEST_METHOD']
 
-        for path, name in self.urls:
-            if path == path_info:
+        for pattern, name in self.urls:
+            m = re.match('^' + pattern + '$', path)
+            if m:
+                # pass the matched groups as arguments to the function
+                args = m.groups()
                 funcname = method.upper() + "_" + name
                 func = getattr(self, funcname)
-                return func()
+                return func(*args)
+
         return self.notfound()
 
     def GET_index(self):
@@ -25,11 +31,11 @@ class application:
         self.start(status, response_headers)
         yield "Welcome!\n"
 
-    def GET_hello(self):
+    def GET_hello(self, name):
         status = '200 OK'
         response_headers = [('Content-type', 'text/plain')]
         self.start(status, response_headers)
-        yield "Hello world!\n"
+        yield "Hello %s!\n" % name
 
     def notfound(self):
         status = '404 Not Found'
